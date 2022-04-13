1795b7e39fe738a9c0cc16d298beeccdf3e9edff
Emiliano Innocenti <innocenti.emiliano@gmail.com>
2022-04-13 10:35:13 +0200

    SlashCommands Scaffolding

---
+++ src/Bot/SlashCommands/DiscordClientExtensions.cs
@@ -0,0 +1,28 @@
+﻿using DSharpPlus;
+using DSharpPlus.Entities;
+using DSharpPlus.SlashCommands;
+
+namespace Ivao.It.DiscordBot.SlashCommands;
+internal static class DiscordClientExtensions
+{
+    internal static DiscordClient UseIvaoSlashCommands(this DiscordClient client)
+    {
+        var slash = client.UseSlashCommands();
+        //slash.RegisterCommands<IvaoSlashCommands>();
+
+        slash.SlashCommandErrored += async (s, e) =>
+        {
+            if (e.Exception is SlashExecutionChecksFailedException slex)
+            {
+                foreach (var check in slex.FailedChecks)
+                    if (check is RequireRoleIdAttribute att)
+                        await e.Context.CreateResponseAsync(
+                            InteractionResponseType.ChannelMessageWithSource,
+                            new DiscordInteractionResponseBuilder().WithContent(
+                                $"You are not allowed to call this Slash Command. You must be a staff member!"));
+            }
+        };
+
+        return client;
+    }
+}
---
+++ src/Bot/SlashCommands/IvaoSlashCommands.cs
@@ -0,0 +1,21 @@
+﻿using DSharpPlus;
+using DSharpPlus.Entities;
+using DSharpPlus.SlashCommands;
+
+namespace Ivao.It.DiscordBot.SlashCommands;
+
+public class IvaoSlashCommands : ApplicationCommandModule
+{
+    [SlashCommand("test", "A slash command made to test the DSharpPlusSlashCommands library!")]
+#if DEBUG
+    [RequireRoleId(771504168610955274)] //Admins
+#else
+    [RequireRoleId(426319218615517184)] //Staff
+#endif
+    public async Task TestCommand(InteractionContext ctx)
+    {
+        await ctx.CreateResponseAsync(
+            InteractionResponseType.ChannelMessageWithSource, 
+            new DiscordInteractionResponseBuilder().WithContent($"Success!"));
+    }
+}
