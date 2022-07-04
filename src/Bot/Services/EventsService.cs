dbf20b12fc9adcc7a46232118fbb3f5304d1e21a
Innocenti Emiliano <e.innocenti@almaviva.it>
2022-07-04 09:40:40 +0200

    Events upcoming notification fixget for the expired tasks only

--- src/Bot/Services/EventsService.cs
+++ src/Bot/Services/EventsService.cs
@@ -14,6 +14,23 @@ internal class EventsService
         _db = ctx;
     }
 
+    /// <summary>
+    /// Gets all the tasks types available
+    /// </summary>
+    /// <returns></returns>
+    //TODO can be cached
+    public async Task<IEnumerable<EventTaskType>> GetAllTaskTypes() 
+        => await this._db.EventTaskTypes.AsNoTracking().ToListAsync();
+
+    /// <summary>
+    /// Adds a new event
+    /// </summary>
+    /// <param name="userId"></param>
+    /// <param name="title"></param>
+    /// <param name="date"></param>
+    /// <param name="tasks"></param>
+    /// <param name="forumLink"></param>
+    /// <returns></returns>
     public async Task<int> AddEventAsync(ulong userId, string title, DateTime date, IEnumerable<EventsTasks> tasks, string? forumLink = null)
     {
         var dbTasks = tasks.Select(t => new EventTask { TaskTypeId = (short)t }).ToList();
@@ -35,12 +52,21 @@ internal class EventsService
         return evt.Id;
     }
 
+    /// <summary>
+    /// Gets an existing event
+    /// </summary>
+    /// <param name="id"></param>
+    /// <returns></returns>
     public async Task<Event?> GetAsync(int id) =>
         await _db.Events.AsNoTracking()
             .Include(p => p.Tasks)
             .ThenInclude(t => t.TaskType)
             .SingleOrDefaultAsync(e => e.Id == id);
 
+    /// <summary>
+    /// Gets all the upcoming events 
+    /// </summary>
+    /// <returns></returns>
     public async Task<ICollection<Event>> GetAsync() =>
         await _db.Events.AsNoTracking()
             .Include(p => p.Tasks)
@@ -48,14 +74,28 @@ internal class EventsService
             .Where(e => e.Date >= DateTime.UtcNow)
             .ToListAsync();
 
-    public async Task<ICollection<Event>> GetUpcomingWithToDosAsync() =>
+    /// <summary>
+    /// Gets upcoming events with tasks expired
+    /// </summary>
+    /// <returns></returns>
+    public async Task<ICollection<Event>> GetUpcomingWithToDosExpiredAsync() =>
         await _db.Events.AsNoTracking()
             .Include(e => e.Tasks)
             .ThenInclude(e => e.TaskType)
             .Where(e => e.Date >= DateTime.UtcNow)
             .Where(e => e.Tasks.Any(t => !t.CompletedAt.HasValue))
+            .Where(e => e.Tasks.Any(t => DateTime.UtcNow >= e.Date.AddDays(-t.TaskType.DaysBefore)))
             .ToListAsync();
 
+    /// <summary>
+    /// Completes a task
+    /// </summary>
+    /// <param name="task"></param>
+    /// <param name="event"></param>
+    /// <param name="userId"></param>
+    /// <param name="content"></param>
+    /// <returns></returns>
+    /// <exception cref="IvaoItBotBusinessException"></exception>
     public async Task CompleteTaskAsync(EventsTasks task, int @event, ulong userId, string? content)
     {
         var taskTypeId = (short)task;
@@ -76,9 +116,20 @@ internal class EventsService
         await _db.SaveChangesAsync();
     }
 
+    /// <summary>
+    /// Checks if task is completed
+    /// </summary>
+    /// <param name="task"></param>
+    /// <param name="event"></param>
+    /// <returns></returns>
     public async Task<bool> IsTaskCompleted(EventsTasks task, int @event) 
         => await _db.EventTasks.AsNoTracking().AnyAsync(t => t.TaskTypeId == (short)task && t.CompletedBy == null);
 
+    /// <summary>
+    /// Deletes an event and all the references
+    /// </summary>
+    /// <param name="eventId"></param>
+    /// <returns></returns>
     public async Task DeleteEventAsync(int eventId)
     {
         var ev = await this._db.Events.SingleOrDefaultAsync(e => e.Id == eventId);
